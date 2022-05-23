3f5c0ac971b55dc1581cd556b54a62a07a10480b
kergely <kergely@gmail.com>
2022-05-23 13:12:22 +0200

    Test changes
    Signed-off-by: kergely <kergely@gmail.com>

--- include/func.hpp
+++ include/func.hpp
@@ -1,5 +1,5 @@
 /*! \file func.hpp
-*   \brief The header file for various mathematical functions
+*   \brief The header file for various mathematical functions, and so on
 */
 
 #include <vector>
--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include "func.hpp"
 
 int main() {
-    double x = 5;
+    double x = 2;
     std::vector<double> a{1.0,0.0,2.0};
     std::cout << Polinom(x,a) << std::endl;
     return 0;
